bf7f7bab96add55140ddd6560e73517999c814c9
Mark Kolich <social@kolich.com>
2013-01-22 00:01:41 -0800

    adding havalo-0.0.4 to downloads and created slick generate-indices.sh script

---
+++ generate-indices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+URL="http://markkolich.github.com/downloads/generate-indices/0.3/generate-indices-0.3-one-jar.jar"
+
+## Check if 'wget' and 'java' exists on the PATH of the running shell.
+command -v wget >/dev/null 2>&1 || { echo >&2 "Sorry, I require wget but it's not on your PATH. Aborting."; exit 1; }
+command -v java >/dev/null 2>&1 || { echo >&2 "Sorry, I require java but it's not on your PATH. Aborting."; exit 1; }
+
+## Jump up one level in the tree.
+cd ..
+
+## Download the indice generation utility.
+wget $URL -O generate-indices.jar
+
+## Generate indices!
+java -jar generate-indices.jar markkolich.github.com
+
+## Cleanup
+rm -f generate-indices.jar
