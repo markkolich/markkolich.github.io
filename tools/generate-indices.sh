295657356e017bb32db20f29522ec38cf45eaa26
Mark Kolich <social@kolich.com>
2013-03-18 10:24:35 -0700

    publishing kolich-common 0.0.7.1

--- tools/generate-indices.sh
+++ tools/generate-indices.sh
@@ -6,8 +6,8 @@ URL="http://markkolich.github.com/downloads/generate-indices/0.3/generate-indice
 command -v wget >/dev/null 2>&1 || { echo >&2 "Sorry, I require wget but it's not on your PATH. Aborting."; exit 1; }
 command -v java >/dev/null 2>&1 || { echo >&2 "Sorry, I require java but it's not on your PATH. Aborting."; exit 1; }
 
-## Jump up one level in the tree.
-cd ..
+## Jump up a few levels in the tree.
+cd ../..
 
 ## Download the indice generation utility.
 wget $URL -O generate-indices.jar
