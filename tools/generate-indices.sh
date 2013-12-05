65a89960046edf79442e5a72db9c68c4fb534ef7
Mark Kolich <social@kolich.com>
2013-12-05 08:27:13 -0800

    Updating Github pages hosted repo to markkolich.github.io (IO ccTLD)

--- tools/generate-indices.sh
+++ tools/generate-indices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="http://markkolich.github.com/downloads/generate-indices/0.3/generate-indices-0.3-one-jar.jar"
+URL="http://markkolich.github.io/downloads/generate-indices/0.3/generate-indices-0.3-one-jar.jar"
 
 ## Check if 'wget' and 'java' exists on the PATH of the running shell.
 command -v wget >/dev/null 2>&1 || { echo >&2 "Sorry, I require wget but it's not on your PATH. Aborting."; exit 1; }
@@ -13,7 +13,7 @@ cd ../..
 wget $URL -O generate-indices.jar
 
 ## Generate indices!
-java -jar generate-indices.jar markkolich.github.com
+java -jar generate-indices.jar markkolich.github.io
 
 ## Cleanup
 rm -f generate-indices.jar
