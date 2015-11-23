1eb4e95f47b6f3c0f8bdbf4c41c0f0286b4370d1
Mark Kolich <social@kolich.com>
2015-11-22 17:56:59 -0800

    Releasing Curacao 4.0.0 - Removing dead versions of Curacao - Removing `tools` directory and moving generate-indicies.sh to root

---
+++ generate-indices.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+URL="http://markkolich.github.io/downloads/generate-indices/0.3/generate-indices-0.3-one-jar.jar"
+
+## Check if 'curl' and 'java' exists on the PATH of the running shell.
+command -v curl >/dev/null 2>&1 || { echo >&2 "Sorry, I require curl but it's not on your PATH. Aborting."; exit 1; }
+command -v java >/dev/null 2>&1 || { echo >&2 "Sorry, I require java but it's not on your PATH. Aborting."; exit 1; }
+
+cd ..
+
+## Download the indice generation utility.
+curl -s $URL -o generate-indices.jar
+
+## Generate indices!
+java -jar generate-indices.jar markkolich.github.io
+
+## Cleanup
+rm -f generate-indices.jar
